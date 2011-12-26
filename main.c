930bd3a4397708320240c84a46a7b6d5f785658d
Montana Rowe <calcnerd256@debian.(none)>
2011-12-25 21:03:32 -0600

    main.c: type byteArray aka barr

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+struct byteArray{
+	size_t size;
+	char* arr;
+}
+
 int poolFree(void* ptr){
 	return free(ptr);
 }
@@ -7,6 +12,18 @@ void* poolAlloc(size_t size){
 	//	Why? Isn't that malloc's job?
 	return malloc(size);//for now
 }
+
+int freeBarr(struct byteArray *arr){
+	poolFree(arr->arr);//TODO: check return value
+	return poolFree(arr);
+}
+struct byteArray *allocBarr(size_t size){
+	struct byteArray *result = (struct byteArray*) poolAlloc(sizeof(struct byteArray));
+	result->arr = poolAlloc(size);
+	result->size = size;
+	return result;
+}
+
 int main(int arfc, char* *arfv){
 	printf("%s", "ok\n");
 	return 0;
