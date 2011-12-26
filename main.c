6c9e30a82c9bccccf8542fef015ec59d1cb15d3b
Montana Rowe <calcnerd256@debian.(none)>
2011-12-25 20:57:54 -0600

    main.c: stubbed pool; main.c main: softened print

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+int poolFree(void* ptr){
+	return free(ptr);
+}
+void* poolAlloc(size_t size){
+	//TODO: use a pool of previously-allocated chunks
+	//	Why? Isn't that malloc's job?
+	return malloc(size);//for now
+}
 int main(int arfc, char* *arfv){
-	printf("ok\n");
+	printf("%s", "ok\n");
 	return 0;
 }
