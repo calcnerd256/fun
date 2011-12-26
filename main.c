c279bdde10c78640134a6e01c78814e8b36ced0e
Montana Rowe <calcnerd256@debian.(none)>
2011-12-25 21:16:35 -0600

    bugfix main.c: include dependencies malloc, terminate struct barr, cascade change type free

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
 struct byteArray{
 	size_t size;
 	char* arr;
-}
+};
 
-int poolFree(void* ptr){
-	return free(ptr);
+void poolFree(void *ptr){
+	free(ptr);
 }
-void* poolAlloc(size_t size){
+void *poolAlloc(size_t size){
 	//TODO: use a pool of previously-allocated chunks
 	//	Why? Isn't that malloc's job?
 	return malloc(size);//for now
 }
 
-int freeBarr(struct byteArray *arr){
-	poolFree(arr->arr);//TODO: check return value
-	return poolFree(arr);
+void freeBarr(struct byteArray *arr){
+	poolFree(arr->arr);//TODO: check error value
+	poolFree(arr);
 }
 struct byteArray *allocBarr(size_t size){
 	struct byteArray *result = (struct byteArray*) poolAlloc(sizeof(struct byteArray));
